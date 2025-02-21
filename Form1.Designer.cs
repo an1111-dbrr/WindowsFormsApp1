3bd42b03ee45611a914ad12f99761f37405bd24d
SPC <SPC@15-LAB402>
2025-02-21 11:07:12 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSo1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSo2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtKQ = new System.Windows.Forms.TextBox();
+            this.btnCong = new System.Windows.Forms.Button();
+            this.btnTru = new System.Windows.Forms.Button();
+            this.btnNhan = new System.Windows.Forms.Button();
+            this.btnChia = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(459, 40);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(336, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ỨNG DỤNG CÁC PHÉP TÍNH CƠ BẢN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(70, 123);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 24);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(72, 105);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "SỐ THỨ NHẤT";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtSo1
+            // 
+            this.txtSo1.Location = new System.Drawing.Point(218, 100);
+            this.txtSo1.Name = "txtSo1";
+            this.txtSo1.Size = new System.Drawing.Size(257, 29);
+            this.txtSo1.TabIndex = 3;
+            this.txtSo1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(72, 154);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 24);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "SỐ THỨ HAI";
+            this.label4.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtSo2
+            // 
+            this.txtSo2.Location = new System.Drawing.Point(218, 149);
+            this.txtSo2.Name = "txtSo2";
+            this.txtSo2.Size = new System.Drawing.Size(257, 29);
+            this.txtSo2.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(72, 205);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 24);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "KẾT QUẢ";
+            this.label5.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtKQ
+            // 
+            this.txtKQ.Location = new System.Drawing.Point(218, 200);
+            this.txtKQ.Name = "txtKQ";
+            this.txtKQ.Size = new System.Drawing.Size(257, 29);
+            this.txtKQ.TabIndex = 3;
+            // 
+            // btnCong
+            // 
+            this.btnCong.Location = new System.Drawing.Point(56, 285);
+            this.btnCong.Name = "btnCong";
+            this.btnCong.Size = new System.Drawing.Size(142, 114);
+            this.btnCong.TabIndex = 4;
+            this.btnCong.Text = "+";
+            this.btnCong.UseVisualStyleBackColor = true;
+            this.btnCong.Click += new System.EventHandler(this.btnCong_Click);
+            // 
+            // btnTru
+            // 
+            this.btnTru.Location = new System.Drawing.Point(218, 285);
+            this.btnTru.Name = "btnTru";
+            this.btnTru.Size = new System.Drawing.Size(142, 114);
+            this.btnTru.TabIndex = 4;
+            this.btnTru.Text = "-";
+            this.btnTru.UseVisualStyleBackColor = true;
+            this.btnTru.Click += new System.EventHandler(this.btnTru_Click);
+            // 
+            // btnNhan
+            // 
+            this.btnNhan.Location = new System.Drawing.Point(384, 285);
+            this.btnNhan.Name = "btnNhan";
+            this.btnNhan.Size = new System.Drawing.Size(142, 114);
+            this.btnNhan.TabIndex = 4;
+            this.btnNhan.Text = "*";
+            this.btnNhan.UseVisualStyleBackColor = true;
+            this.btnNhan.Click += new System.EventHandler(this.btnNhan_Click);
+            // 
+            // btnChia
+            // 
+            this.btnChia.Location = new System.Drawing.Point(553, 285);
+            this.btnChia.Name = "btnChia";
+            this.btnChia.Size = new System.Drawing.Size(142, 114);
+            this.btnChia.TabIndex = 4;
+            this.btnChia.Text = "/";
+            this.btnChia.UseVisualStyleBackColor = true;
+            this.btnChia.Click += new System.EventHandler(this.btnChia_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1467, 831);
+            this.Controls.Add(this.btnChia);
+            this.Controls.Add(this.btnNhan);
+            this.Controls.Add(this.btnTru);
+            this.Controls.Add(this.btnCong);
+            this.Controls.Add(this.txtKQ);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtSo2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSo1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSo1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSo2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtKQ;
+        private System.Windows.Forms.Button btnCong;
+        private System.Windows.Forms.Button btnTru;
+        private System.Windows.Forms.Button btnNhan;
+        private System.Windows.Forms.Button btnChia;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCong_Click(object sender, EventArgs e)
+        {
+            double so1 = Convert.ToDouble(txtSo1.Text);
+            double so2 = Convert.ToDouble(txtSo2.Text);
+
+            double kq = so1 + so2;
+            txtKQ.Text = kq.ToString();
+        }
+
+        private void btnTru_Click(object sender, EventArgs e)
+        {
+            double so1 = Convert.ToDouble(txtSo1.Text);
+            double so2 = Convert.ToDouble(txtSo2.Text);
+
+            double kq = so1 - so2;
+            txtKQ.Text = kq.ToString();
+        }
+
+        private void btnNhan_Click(object sender, EventArgs e)
+        {
+            double so1 = Convert.ToDouble(txtSo1.Text);
+            double so2 = Convert.ToDouble(txtSo2.Text);
+
+            double kq = so1 * so2;
+            txtKQ.Text = kq.ToString();
+        }
+
+        private void btnChia_Click(object sender, EventArgs e)
+        {
+            double so1 = Convert.ToDouble(txtSo1.Text);
+            double so2 = Convert.ToDouble(txtSo2.Text);
+
+
+
+            if (so2 == 0)
+            {
+                // báo lỗi không thể chia cho 0
+                txtKQ.Text = " không thể chia cho 0";
+            }
+            else
+            { // ngược lại chia và hiện kết quả                   
+                double kq = so1 / so2;                   
+                txtKQ.Text = kq.ToString();
+            }
+        }
+    }
+}
